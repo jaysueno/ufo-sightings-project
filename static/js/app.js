666227ac0543a86854f961deb2c421c83346bae1
Jay Sueno <jay.sueno@gmail.com>
2020-09-08 20:34:40 -0700

    started work on code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
 
 // YOUR CODE HERE!
+console.log(data)
+
+// create listeners for both the entry form and the button
+// these variables will "listen" to an action that happens to that html element
+var button = d3.select("#filter-btn");
+console.log(button.html())
+var form = d3.select(".form-control");
+console.log(form.html())
+
+// create "event handlers" for each of the button and the form
+// ".on('event', function)" will run a function based on an event that happens with the listener variable
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// create the function for the event handler
+function runEnter() {
+    
+    // prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // log with action dispatched the event
+    console.log(d3.event.target)
+
+    // store the event in a variable
+    var inputValue = form.property("value");
+    console.log(`filtered form value: ${inputValue}`);
+    var clickValue = button.property("value");
+    console.log(`clicked button ${clickValue}`);
+
+
+}
