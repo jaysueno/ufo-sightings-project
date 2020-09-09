15fe71a4ea91a57544093940032af9e05a15cb99
Jay Sueno <jay.sueno@gmail.com>
2020-09-08 22:16:05 -0700

    finished commenting on level one code

--- static/js/app.js
+++ static/js/app.js
@@ -30,22 +30,35 @@ function runEnter() {
 
     // store the event in a variable
     var inputValue = form.property("value");
+    // log the input value to console
     console.log(`filtered form value: ${inputValue}`);
+
     var clickValue = button.property("value");
     console.log(`clicked button ${clickValue}`);
 
-    // filter the data to correspond to the date selected
+    // use .filter() function to filter the data to correspond to the date selected
+    // we use the arrow function "=>" to minimize our script for elegance 
+    // we use a boolean "===" to check for equality and object type
     var filteredUfo = ufoData.filter(sighting => sighting.datetime == inputValue);
+    // log the filtered data to the console
     console.log(filteredUfo);
 
+    // loop function. use ".forEach()" function to iterate through the array of objects
+    // with each iteration we add a row and cells from the filtered data
+    // we use the arrow function for elegance 
     filteredUfo.forEach(ufo => {
         console.log(ufo);
+        // with each iteration we append a row "tr" to the table body element/node and store it as a variable "row"
         var row = table.append("tr");
 
+        // we use "Object.entries()" function to break out the key and value of each entry in the object
+        // we iterate through the array of entries with the .forEach() function to populate the HTML table with our script
         Object.entries(ufo).forEach(([key, value]) => {
             console.log(key, value);
 
+            // we use the .append() function to add a "cell" element "td" to the row
             var cell = row.append("td");
+            // for each "cell" we will write the value of the iterated object "value"
             cell.text(value);
         })
     })
